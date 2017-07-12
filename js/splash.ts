decf36b788688d258bc400dd044dc210921dd24e
Ryan Petschek <petschekr@gmail.com>
2017-07-12 12:01:20 -0400

    Massively simplify JS generation

--- js/splash.ts
+++ js/splash.ts
@@ -1,15 +1,14 @@
-function wait(milliseconds) {
+function wait(milliseconds: number) {
     return new Promise((resolve, reject) => {
         setTimeout(resolve, milliseconds);
     });
 }
 
-async function writeText(container) {
+async function writeText(container: HTMLElement) {
     await wait(500);
 
     const introText = [
         "Initializing......",
-        "Well, here we are.",
         "Welcome to HackGT: New Heights"
     ];
     for (let line of introText) {
@@ -33,11 +32,10 @@ async function writeText(container) {
 window.onload = async () => {
     "use strict";
     
-    
     document.body.addEventListener("keypress", (event) => {
         document.getElementsByClassName("cover")[0].classList.add("hidden");
     });
     
-    await writeText(document.getElementById("intro-text"));
+    await writeText(document.getElementById("intro-text")!);
     document.getElementsByClassName("cover")[0].classList.add("hidden");
 };
