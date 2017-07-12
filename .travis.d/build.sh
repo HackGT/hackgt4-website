c8534816cb91c2ee1f925d7af637c38c83067bf9
Michael Eden <themichaeleden@gmail.com>
2017-07-11 22:30:14 -0700

    Added hackgproject config.

---
+++ .travis.d/build.sh
@@ -0,0 +1,193 @@
+#!/usr/bin/env bash
+# HACKGPROJECT VERSION: 33d73fdf72d09777c5ff05de18cf394405da5779
+set -euo pipefail
+PROJECT_TYPE="static"
+ORG_NAME="HackGT"
+SOURCE_DIR=$(readlink -f "${BASH_SOURCE[0]}")
+SOURCE_DIR=$(dirname "$SOURCE_DIR")
+cd "${SOURCE_DIR}/.."
+set -x
+
+if ! hash docker &>/dev/null; then
+    echo "Cannot find `docker`!" >&2
+    exit 64
+fi
+
+docker=
+if docker ps &>/dev/null; then
+    docker=docker
+else
+    docker='sudo docker'
+fi
+
+remote=$(git remote -v | grep -Pio "${ORG_NAME}"'/[a-zA-Z0-9-_\.]*' | head -1)
+image_name=$(basename "${remote%.*}")
+
+build_project_source() {
+    if [[ -f Dockerfile.build ]]; then
+        local build_image_name="$(basename $(pwd))-build"
+        $docker build -f Dockerfile.build --rm -t "$build_image_name" .
+        $docker run -w '/src' -v "$(pwd):/src" "$build_image_name"
+    fi
+}
+
+test_project_source() {
+    if [[ -f Dockerfile.test ]]; then
+        local test_image_name="$(basename $(pwd))-test"
+        $docker build -f Dockerfile.test --rm -t "$test_image_name" .
+        $docker run -w '/src' -v "$(pwd):/src" "$test_image_name"
+    fi
+}
+
+build_project_container() {
+    $docker build -f Dockerfile --rm -t "$image_name" .
+}
+
+publish_project_container() {
+    local git_rev=$(git rev-parse HEAD)
+    local branch=$(git rev-parse --abbrev-ref HEAD)
+    local latest_tag_name="latest"
+    local push_image_name="${DOCKER_ID_USER}/${image_name}"
+    if [[ $branch != master ]]; then
+        latest_tag_name="latest-${branch}"
+    fi
+    docker login -u="${DOCKER_ID_USER}" -p="${DOCKER_PASSWORD}"
+    docker tag "$image_name" "$push_image_name":"$git_rev"
+    docker push "$push_image_name"
+    docker tag "$push_image_name":"$git_rev" "$push_image_name":"$latest_tag_name"
+    docker push "$push_image_name"
+}
+
+trigger_biodomes_build() {
+    body='{
+    "request": {
+    "branch":"master"
+    } }'
+
+    curl -s -X POST \
+       -H "Content-Type: application/json" \
+       -H "Accept: application/json" \
+       -H "Travis-API-Version: 3" \
+       -H "Authorization: token ${TRAVIS_TOKEN}" \
+       -d "$body" \
+       https://api.travis-ci.org/repo/${ORG_NAME}%2Fbiodomes/requests
+}
+
+install_jekyll() {
+    gem install jekyll
+    bundle install
+}
+
+build_jekyll() {
+    bundle exec jekyll build
+}
+
+commit_to_branch() {
+    local branch="${1:-gh-pages}"
+    local git_rev=$(git rev-parse --short HEAD)
+    git config user.name 'Michael Eden'
+    git config user.email 'themichaeleden@gmail.com'
+    git fetch origin
+    git reset "origin/$branch" || git checkout -b "$branch"
+    git add -A .
+    git status
+    git commit -m "Automatic Travis deploy of ${git_rev}."
+    git push -q origin "HEAD:${branch}"
+}
+
+set_cloudflare_dns() {
+    local type="$1"
+    local name="$2"
+    local content="$3"
+    local proxied="$4"
+    local type_downcase=$(echo ${type} | tr '[:upper:]' '[:lower:]')
+    local name_downcase=$(echo ${name} | tr '[:upper:]' '[:lower:]')
+    local content_downcase=$(echo ${content} | tr '[:upper:]' '[:lower:]')
+
+    # get all the dns records
+    local dns_records=$(curl -X GET \
+          -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
+          -H "X-Auth-Key: ${CLOUDFLARE_AUTH}" \
+          -H "Content-Type: application/json" \
+          "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/dns_records" \
+          | tr '[:upper:]' '[:lower:]')
+
+    # Check if we already set it
+    local jq_exists=$(cat <<-END
+        .result[]
+        | select(.type == "${type_downcase}")
+        | select(.name == "${name_downcase}")
+        | select(.content == "${content_downcase}")
+END
+    )
+    if [[ -n $(echo "${dns_records}" | jq "${jq_exists}") ]]; then
+        echo "Record already set, not setting again."
+        return
+    fi
+
+    # Check if there's a different one already set
+    local duplicate_exists=$(echo "${dns_records}" \
+        | jq '.result[] | select(.name == "${name_downcase}")')
+    if [[ -n $duplicate_exists ]]; then
+        echo "Record with the same host exists, will not overwrite!"
+        exit 64
+    fi
+
+    # Set IT!
+    local dns_record=$(cat <<-END
+        {
+            "type": "${type}",
+            "name": "${name}",
+            "content": "${content}",
+            "proxied": $proxied
+        }
+END
+    )
+    local dns_success=$(curl -X POST \
+         --data "$dns_record" \
+         -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
+         -H "X-Auth-Key: ${CLOUDFLARE_AUTH}" \
+         -H "Content-Type: application/json" \
+         "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/dns_records")
+
+    if [[ $dns_success != true ]]; then
+        echo 'DNS Setting on cloudflare failed!!'
+        echo 'CloudFlare output:'
+        echo "$dns_success"
+        exit 64
+    fi
+    echo DNS set! You\'ll have to wait a bit to see the changes!
+}
+
+
+deployment_project() {
+    build_project_source
+    test_project_source
+    build_project_container
+
+    if [[ ${TRAVIS_PULL_REQUEST:-} = false ]]; then
+        publish_project_container
+        trigger_biodomes_build
+    fi
+}
+
+static_project() {
+    # If there's anything we want to do to build it,
+    # do it now. (if we're using something other than jekyll)
+    build_project_source
+
+    if [[ ${TRAVIS_BRANCH:-} = master && ${TRAVIS_PULL_REQUEST:-} = false ]]; then
+        commit_to_branch 'gh-pages'
+        set_cloudflare_dns CNAME "$(cat CNAME)" "${ORG_NAME}.github.io" true
+    fi
+}
+
+
+
+if [[ $PROJECT_TYPE = deployment ]]; then
+    deployment_project
+fi
+
+if [[ $PROJECT_TYPE = static ]]; then
+    static_project
+fi
---
+++ README.md
@@ -0,0 +1,2 @@
+# hackg4website [![Build Status](https://travis-ci.org/hackgt/hackg4website.svg?branch=master)](https://travis-ci.org/hackgt/hackg4website)
+
